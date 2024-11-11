e4f6a428255c7d6189c3f4d7407ab312f5dbb3f6
HP <HP@LAPTOP-P7GJTODP>
2024-11-11 17:09:31 +0700

    Them dang nhap dang ki

---
+++ GaubongShop/Models/ViewModel/RegisterVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GaubongShop.Models.ViewModel
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "Username cannot be blank.")]
+        [Display(Name = "Tên đăng nhập")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password cannot be blank.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Xác nhận mật khẩu")]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password cannot match.")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [Display(Name = "Họ tên")]
+        public string CustomerName { get; set; }
+        [Required]
+        [Display(Name = "Số điện thoại")]
+        [DataType(DataType.PhoneNumber)]
+        public string CustomerPhone { get; set; }
+        [Required(ErrorMessage = "Email cannot be blank.")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string CustomerEmail { get; set; }
+        [Required]
+        [Display(Name = "Địa chỉ")]
+        public string CustomerAddress { get; set; }
+    }
+
+
+}
