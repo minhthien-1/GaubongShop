22c413c4f278709ade8b2f95729900a5279f013d
Lê Thanh Trúc <23DH113824@st.huflit.edu.vn>
2024-11-13 12:29:02 +0700

    truc

--- GaubongShop/Areas/Admin/Controllers/ProductsController.cs
+++ GaubongShop/Areas/Admin/Controllers/ProductsController.cs
@@ -15,39 +15,72 @@ namespace GaubongShop.Areas.Admin.Controllers
     public class ProductsController : Controller
     {
         private GauBongStoreEntities db = new GauBongStoreEntities();
-
-        // GET: Admin/Products
-        public ActionResult Index(string searchTerm, string sortOrder, int? page)
+        public ActionResult ProductList(int? category, int? page, string SearchString, double min = double.MinValue, double max = double.MaxValue)
         {
-            var model = new ProductSearchVM();
-            var products = db.Products.AsQueryable();
-            if (!string.IsNullOrEmpty(searchTerm))
+            var products = db.Products.Include(p => p.Category);
+            // Tìm kiếm chuỗi truy vấn theo category
+            if (category == null)
             {
-                model.SearchTerm = searchTerm;
-                //tìm kiếm sản phẩm dựa trên từ khóa
-                products = products.Where(p =>
-                p.ProductName.Contains(searchTerm) ||
-                p.ProductDecription.Contains(searchTerm) ||
-                p.Category.CategoryName.Contains(searchTerm));
+                products = db.Products.OrderByDescending(x => x.ProductName);
             }
-            //Áp dụng sắp xếp dựa trên lựa chọn của người dùng
-            switch (sortOrder)
+            else
             {
-                case "name_asc": products = products.OrderBy(p => p.ProductName); break;
-                case "name_desc": products = products.OrderByDescending(p => p.ProductName); break;
-                case "price_asc": products = products.OrderBy(p => p.ProductPrice); break;
-                case "price_desc": products = products.OrderByDescending(p => p.ProductPrice); break;
-                default: //Mặc định sắp xếp theo tên
-                    products = products.OrderBy(p => p.ProductName); break;
+                products = db.Products.OrderByDescending(x => x.CategoryID).Where(x => x.CategoryID == category);
             }
-            model.SortOrder = sortOrder;
-            //Đoạn code liên quan đến phân trang
-            //Lấy số hiện tại (mặc định là trang 1 nếu không có giá trị)
-            int pageNumber = page ?? 1;
-            int pageSize = 4; /*số sản phẩm mỗi trang*/
-            model.Products = products.ToPagedList(pageNumber, pageSize);
-            return View(model);
+            // Tìm kiếm chuỗi truy vấn theo NamePro (SearchString)
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                products = db.Products.OrderByDescending(x => x.CategoryID).Where(s => s.ProductName.Contains(SearchString.Trim()));
+            }
+            // Tìm kiếm chuỗi truy vấn theo đơn giá
+            if (min >= 0 && max > 0)
+            {
+                products = db.Products.OrderByDescending(x => x.ProductPrice).Where(p => (double)p.ProductPrice >= min && (double)p.ProductPrice <= max);
+            }
+            // Khai báo mỗi trang 4 sản phẩm
+            int pageSize = 4;
+            // Toán tử ?? trong C# mô tả nếu page khác null thì lấy giá trị page, còn
+            // nếu page = null thì lấy giá trị 1 cho biến pageNumber.
+            int pageNumber = (page ?? 1);
+
+            // Nếu page = null thì đặt lại page là 1.
+            if (page == null) page = 1;
+
+            // Trả về các product được phân trang theo kích thước và số trang.
+            return View(products.ToPagedList(pageNumber, pageSize));
         }
+        // GET: Admin/Products
+        //public ActionResult Index(string searchTerm, string sortOrder, int? page)
+        //{
+        //    var model = new ProductSearchVM();
+        //    var products = db.Products.AsQueryable();
+        //    if (!string.IsNullOrEmpty(searchTerm))
+        //    {
+        //        model.SearchTerm = searchTerm;
+        //        //tìm kiếm sản phẩm dựa trên từ khóa
+        //        products = products.Where(p =>
+        //        p.ProductName.Contains(searchTerm) ||
+        //        p.ProductDecription.Contains(searchTerm) ||
+        //        p.Category.CategoryName.Contains(searchTerm));
+        //    }
+        //    //Áp dụng sắp xếp dựa trên lựa chọn của người dùng
+        //    switch (sortOrder)
+        //    {
+        //        case "name_asc": products = products.OrderBy(p => p.ProductName); break;
+        //        case "name_desc": products = products.OrderByDescending(p => p.ProductName); break;
+        //        case "price_asc": products = products.OrderBy(p => p.ProductPrice); break;
+        //        case "price_desc": products = products.OrderByDescending(p => p.ProductPrice); break;
+        //        default: //Mặc định sắp xếp theo tên
+        //            products = products.OrderBy(p => p.ProductName); break;
+        //    }
+        //    model.SortOrder = sortOrder;
+        //    //Đoạn code liên quan đến phân trang
+        //    //Lấy số hiện tại (mặc định là trang 1 nếu không có giá trị)
+        //    int pageNumber = page ?? 1;
+        //    int pageSize = 4; /*số sản phẩm mỗi trang*/
+        //    model.Products = products.ToPagedList(pageNumber, pageSize);
+        //    return View(model);
+        //}
         // GET: Admin/Products/Details/5
         public ActionResult Details(int? id)
         {
