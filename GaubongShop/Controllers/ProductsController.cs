167102dbcd2917e323e5ad53bb45e0fa227bf83b
tnct3000@gmail.com <tnct3000@gmail.com>
2024-11-13 14:06:02 +0700

    sua loi cua Viet

--- GaubongShop/Controllers/ProductsController.cs
+++ GaubongShop/Controllers/ProductsController.cs
@@ -7,81 +7,21 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using GaubongShop.Models;
-using GaubongShop.Models.ViewModel;
-using PagedList;
 
-namespace GaubongShop.Areas.Admin.Controllers
+namespace GaubongShop.Controllers
 {
     public class ProductsController : Controller
     {
         private GauBongStoreEntities db = new GauBongStoreEntities();
-        public ActionResult ProductList(int? category, int? page, string SearchString, double min = double.MinValue, double max = double.MaxValue)
+
+        // GET: Products
+        public ActionResult Index()
         {
             var products = db.Products.Include(p => p.Category);
-            // Tìm kiếm chuỗi truy vấn theo category
-            if (category == null)
-            {
-                products = db.Products.OrderByDescending(x => x.ProductName);
-            }
-            else
-            {
-                products = db.Products.OrderByDescending(x => x.CategoryID).Where(x => x.CategoryID == category);
-            }
-            // Tìm kiếm chuỗi truy vấn theo NamePro (SearchString)
-            if (!String.IsNullOrEmpty(SearchString))
-            {
-                products = db.Products.OrderByDescending(x => x.CategoryID).Where(s => s.ProductName.Contains(SearchString.Trim()));
-            }
-            // Tìm kiếm chuỗi truy vấn theo đơn giá
-            if (min >= 0 && max > 0)
-            {
-                products = db.Products.OrderByDescending(x => x.ProductPrice).Where(p => (double)p.ProductPrice >= min && (double)p.ProductPrice <= max);
-            }
-            // Khai báo mỗi trang 4 sản phẩm
-            int pageSize = 4;
-            // Toán tử ?? trong C# mô tả nếu page khác null thì lấy giá trị page, còn
-            // nếu page = null thì lấy giá trị 1 cho biến pageNumber.
-            int pageNumber = (page ?? 1);
-
-            // Nếu page = null thì đặt lại page là 1.
-            if (page == null) page = 1;
-
-            // Trả về các product được phân trang theo kích thước và số trang.
-            return View(products.ToPagedList(pageNumber, pageSize));
+            return View(products.ToList());
         }
-        //GET: Admin/Products
-        public ActionResult Index(string searchTerm, string sortOrder, int? page)
-        {
-            var model = new ProductSearchVM();
-            var products = db.Products.AsQueryable();
-            if (!string.IsNullOrEmpty(searchTerm))
-            {
-                model.SearchTerm = searchTerm;
-                //tìm kiếm sản phẩm dựa trên từ khóa
-                products = products.Where(p =>
-                p.ProductName.Contains(searchTerm) ||
-                p.ProductDecription.Contains(searchTerm) ||
-                p.Category.CategoryName.Contains(searchTerm));
-            }
-            //Áp dụng sắp xếp dựa trên lựa chọn của người dùng
-            switch (sortOrder)
-            {
-                case "name_asc": products = products.OrderBy(p => p.ProductName); break;
-                case "name_desc": products = products.OrderByDescending(p => p.ProductName); break;
-                case "price_asc": products = products.OrderBy(p => p.ProductPrice); break;
-                case "price_desc": products = products.OrderByDescending(p => p.ProductPrice); break;
-                default: //Mặc định sắp xếp theo tên
-                    products = products.OrderBy(p => p.ProductName); break;
-            }
-            model.SortOrder = sortOrder;
-            //Đoạn code liên quan đến phân trang
-            //Lấy số hiện tại (mặc định là trang 1 nếu không có giá trị)
-            int pageNumber = page ?? 1;
-            int pageSize = 4; /*số sản phẩm mỗi trang*/
-            model.Products = products.ToPagedList(pageNumber, pageSize);
-            return View(model);
-        }
-       // GET: Admin/Products/Details/5
+
+        // GET: Products/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -96,14 +36,14 @@ namespace GaubongShop.Areas.Admin.Controllers
             return View(product);
         }
 
-        // GET: Admin/Products/Create
+        // GET: Products/Create
         public ActionResult Create()
         {
             ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
             return View();
         }
 
-        // POST: Admin/Products/Create
+        // POST: Products/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -121,7 +61,7 @@ namespace GaubongShop.Areas.Admin.Controllers
             return View(product);
         }
 
-        // GET: Admin/Products/Edit/5
+        // GET: Products/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -137,7 +77,7 @@ namespace GaubongShop.Areas.Admin.Controllers
             return View(product);
         }
 
-        // POST: Admin/Products/Edit/5
+        // POST: Products/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -154,7 +94,7 @@ namespace GaubongShop.Areas.Admin.Controllers
             return View(product);
         }
 
-        // GET: Admin/Products/Delete/5
+        // GET: Products/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -169,7 +109,7 @@ namespace GaubongShop.Areas.Admin.Controllers
             return View(product);
         }
 
-        // POST: Admin/Products/Delete/5
+        // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
