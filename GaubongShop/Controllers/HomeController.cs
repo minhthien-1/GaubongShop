1535211c0fd1e98f4276882f4b360d7ace5983ca
tnct3000@gmail.com <tnct3000@gmail.com>
2024-11-13 16:07:08 +0700

    them controller va cac phan chi tiet san pham

--- GaubongShop/Controllers/HomeController.cs
+++ GaubongShop/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Web.UI;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using GaubongShop.Models;
+using GaubongShop.Models.ViewModel;
+using PagedList;
+using System.Linq;
 
 namespace GaubongShop.Controllers
 {
     public class HomeController : Controller
     {
+        private GauBongStoreEntities db = new GauBongStoreEntities();
         public ActionResult Index()
         {
             return View();
@@ -29,6 +33,33 @@ namespace GaubongShop.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult ProductDetails(int?id, int?quantity, int? page)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product pro = db.Products.Find(id);
+            if(pro == null )
+            {
+                return HttpNotFound();
+            }
+            //lấy tất cả các sản phẩm cùng danh mục
+            var products = db.Products.Where(p=>p.CategoryID==pro.CategoryID &&p.ProductID!=pro.ProductID).AsQueryable();
+            ProductDetailVM model = new ProductDetailVM();
+            //Đoạn code liên quan tới phân trang 
+            //Lấy số trang hiện tại (mặc định là trang 1 nếu trang không có giá trị)
+            int pageNumber = page ?? 1;
+            int pageSize = model.PageSize; //số sản phẩm mỗi trang
+            model.product = pro;
+            model.RealatedProducts = products.OrderBy(p => p.ProductID).Take(pageSize).ToList();//cần sửa lỗi 
+            model.TopProducts = products.OrderByDescending(p=>p.OrderDetails.Count()).Take(3).ToPagedList(pageNumber, pageSize);
+            if (quantity.HasValue)
+            {
+                model.quantity = quantity.Value;
+            }
+            return View(model);
         }
     }
 }
---
+++ GaubongShop/Models/ViewModel/ProductDetailVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+using PagedList.Mvc;
+namespace GaubongShop.Models.ViewModel
+{
+    public class ProductDetailVM
+    {
+        public Product product { get; set; }
+        public int quantity { get; set; } = 1;
+        //tinh gia tri tam thoi 
+        public decimal estimatedValue => quantity * product.ProductPrice;
+        //Các thuộc tính hỗ trợ phân trang 
+        public int PageNumber { get; set; } //trang hiện tại
+        public int PageSize { get; set; } //số sản phẩm mỗi trang 
+        //Danh sách 3 sản phẩm cùng danh mục
+        public List<Product> RealatedProducts { get; set;}
+        //Danh sách 3 sản phẩm nổi bật 
+        public PagedList.IPagedList<Product> TopProducts { get; set; }  
+    }
+}
