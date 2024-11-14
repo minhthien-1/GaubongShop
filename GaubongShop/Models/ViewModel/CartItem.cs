8fb0a22c45b2d222d637673c9295fe550b088d0e
tnct3000@gmail.com <tnct3000@gmail.com>
2024-11-14 07:54:34 +0700

    sss

---
+++ GaubongShop/Models/ViewModel/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace GaubongShop.Models.ViewModel
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }  
+        public string ProductName { get;set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get;set; }
+        public string ProductImage { get;set; }
+        //Tổng giá cho mọi sản phẩm
+        public decimal TotalPrice => Quantity*UnitPrice;
+    }
+}
