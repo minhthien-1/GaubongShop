4d23cce5a7afb2068176f9594619f077e74293f5
tnct3000@gmail.com <tnct3000@gmail.com>
2024-11-23 21:36:26 +0700

    suavathemdb

--- GaubongShop/Models/Model1.Designer.cs
+++ GaubongShop/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\du an nhom\GaubongShop\GaubongShop\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\thuchanhnhomchaythu\GaubongShop\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
