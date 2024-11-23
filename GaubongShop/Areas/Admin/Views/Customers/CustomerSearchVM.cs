38877426a8f2f5ce815e4fc782fa78bc8dd375b5
Lê Thanh Trúc <23DH113824@st.huflit.edu.vn>
2024-11-23 23:17:38 +0700

    truc

---
+++ GaubongShop/Areas/Admin/Views/Customers/CustomerSearchVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GaubongShop.Areas.Admin.Views.Customers
+{
+    public class CustomerSearchVM
+    {
+        public string SearchTerm { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; } = 10;
+        public CustomerSearchVM() { }
+    }
+}
